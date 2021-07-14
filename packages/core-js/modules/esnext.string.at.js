Oksana-Hubenko89/core-js

replace `esnext.string.at` by `esnext.string.at-alternative`

packages/core-js/modules/esnext.string.at.js
@@ -1,16 +1,22 @@
 'use strict';
 var $ = require('../internals/export');
-var charAt = require('../internals/string-multibyte').charAt;
+var requireObjectCoercible = require('../internals/require-object-coercible');
+var toLength = require('../internals/to-length');
+var toInteger = require('../internals/to-integer');
 var fails = require('../internals/fails');
 
 var FORCED = fails(function () {
-  return '𠮷'.at(0) !== '𠮷';
+  return '𠮷'.at(0) !== '\uD842';
 });
 
 // `String.prototype.at` method
-// https://github.com/mathiasbynens/String.prototype.at
+// https://github.com/tc39/proposal-relative-indexing-method
 $({ target: 'String', proto: true, forced: FORCED }, {
-  at: function at(pos) {
-    return charAt(this, pos);
+  at: function at(index) {
+    var S = String(requireObjectCoercible(this));
+    var len = toLength(S.length);
+    var relativeIndex = toInteger(index);
+    var k = relativeIndex >= 0 ? relativeIndex : len + relativeIndex;
+    return (k < 0 || k >= len) ? undefined : S.charAt(k);
   }
 });

zloirock/core.js

packages/core-js/modules/esnext.string.at.js
@@ -1,16 +1,23 @@
 'use strict';
 var $ = require('../internals/export');
-var charAt = require('../internals/string-multibyte').charAt;
+var requireObjectCoercible = require('../internals/require-object-coercible');
+var toInteger = require('../internals/to-integer');
+var toLength = require('../internals/to-length');
+var toString = require('../internals/to-length');
 var fails = require('../internals/fails');
 
 var FORCED = fails(function () {
-  return '𠮷'.at(0) !== '𠮷';
+  return '𠮷'.at(0) !== '\uD842';
 });
 
 // `String.prototype.at` method
-// https://github.com/mathiasbynens/String.prototype.at
+// https://github.com/tc39/proposal-relative-indexing-method
 $({ target: 'String', proto: true, forced: FORCED }, {
-  at: function at(pos) {
-    return charAt(this, pos);
+  at: function at(index) {
+    var S = toString(requireObjectCoercible(this));
+    var len = toLength(S.length);
+    var relativeIndex = toInteger(index);
+    var k = relativeIndex >= 0 ? relativeIndex : len + relativeIndex;
+    return (k < 0 || k >= len) ? undefined : S.charAt(k);
   }
 });